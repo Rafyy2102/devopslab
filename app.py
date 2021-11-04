1633af9047cbfac6e96794230aaf01dd9ab0430f
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-11-03 21:34:19 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ def pagina_inicial():
     return "Oi Rafyy belezinha"
 
 #PARA SIMULAR UM DEBUG
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
+#@app.route('/bug')                                                                                                                                
+#def bad():                                                                                                                                        
+#    try:                                                                                                                                          
+#        raise TypeError()                                                                                                                         
+#    except TypeError as e:                                                                                                                        
+#        print(e)                                                                                                                                  
+#    except TypeError as e:                                                                                                                        
+#        print("Duplicado, ou seja, nunca vai entrar aqui.")    
 
 
 #Criando um novo endPoint
