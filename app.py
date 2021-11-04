e7f998d4710434a931658562243e6570886eb153
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-11-03 21:32:35 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Oi Rafyy belezinha"
 
+#PARA SIMULAR UM DEBUG
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
+
+
 #Criando um novo endPoint
 @app.route("/soma")
 def soma_valores():
